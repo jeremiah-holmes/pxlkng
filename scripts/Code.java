5ea2692f2e2f0cc032cde2ff2504b75a4d42ed2a
pxlkng <41935466+pxlkng@users.noreply.github.com>
2018-08-02 11:39:26 -0500

    El Maguey

---
+++ scripts/Code.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Code extends JApplet {
+	public void paint (Graphics g) {
+		super.paint(g);
+		g.drawString("wow this actually works?", 25, 25);
+	}
+}
